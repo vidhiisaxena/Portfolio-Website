9f856911d632e6ba7c20165d4ce3d468f68afe33
Vidhi Saxena <144513634+vidhiisaxena@users.noreply.github.com>
2024-12-10 23:29:36 +0530

    Add files via upload

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,1 +1,57 @@
+import project1 from "../assets/projects/project-1.jpg";
+import project2 from "../assets/projects/project-2.jpg";
 
+export const HERO_CONTENT = `I am a front-end developer skilled in HTML, CSS, JavaScript, Tailwind CSS, and React, with a focus on creating intuitive and engaging user interfaces. Proficient in UI/UX design, I craft seamless digital experiences. Currently exploring AI and ML, I utilize Python libraries and Frameworks to develop innovative, data-driven solutions, blending creativity with technical expertise.`;
+
+export const EXPERIENCES = [
+  {
+    year: "Dec 2024 - Present",
+    role: "Machine Learning Researcher",
+    company: "Defence Research and Development Organization",
+    description: `Worked under DRDO at CFEES Lab in the field of Machine Learning, applying algorithms for optimisation processes for the laboratory purposes.`,
+    technologies: ["Python", "TensorFlow", "PyTorch", "Matplotlib", "LLMs"],
+  },
+  {
+    year: "Oct 2024 - Present",
+    role: "AWS Cloud Club Mentor",
+    company: "AWS Cloud Club IGDTUW",
+    description: `Mentored a team of 5 mentees to develope AQI-Predictor using Machine Learning Algorithms and Time Series and conducted cohort session on Web Development.`,
+    technologies: ["Python", "XGBoost", "HTML", "CSS", "Bootstrap"],
+  },
+  {
+    year: "June 2024 - August 2024",
+    role: "Python and Machine Learning Intern",
+    company: "Anveshan Foundation IGDTUW",
+    description: `Designed and evaluated machine learning models for
+network anomaly detection in IoT devices, optimizing
+accuracy with feature selection and multi-metric analysis.`,
+    technologies: [
+      "Python",
+      "Machine-Learning",
+      "Extrapolitary Data Analysis",
+      "Streamlit",
+    ],
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "CNN Image Classifier with Pretrained Models",
+    image: project1,
+    description:
+      "project implements an image classifier using three popular Convolutional Neural Network (CNN) architectures: ResNet-18, AlexNet, and VGG16. The models are pretrained on the ImageNet dataset and can classify images into one of 1,000 ImageNet classes. Users can run the classifier locally, choose their preferred CNN architecture, and evaluate the model's predictions.",
+    technologies: ["Python", "CNN", "PIL", "Keras"],
+  },
+  {
+    title: "Portfolio Website",
+    image: project2,
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["HTML", "CSS" , "React", "Tailwind CSS"],
+  },
+];
+
+export const CONTACT = {
+  address: "Ghaziabad, Uttar Pradesh, Delhi-NCR",
+  email: "vidhiisaxena076@gmail.com",
+};
