9cd11689be6db716be293422f92b13796362bff9
Vidhi Saxena <144513634+vidhiisaxena@users.noreply.github.com>
2024-12-10 23:30:44 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import About from './components/About';
+import Technologies from './components/technologies';
+import Experience from './components/Experience';
+import Projects from "./components/projects";
+
+const App = () => {
+  return (
+    <div className="overflow-x-hidden text-amber-900 antialiased selection:bg-amber-900 selection:text-orange-200">
+      <div className="fixed top-0 -z-10 h-full w-full">
+        <div class="bg-fixed absolute top-0 z-[-2] h-screen w-screen rotate-180 transform bg-fixed bg-white bg-[radial-gradient(60%_120%_at_50%_50%,hsla(0,0%,100%,0)_0,rgba(250,237,205,.5)_100%)]"></div>
+      </div>
+      <div className="container mx-auto px-8">
+        <Navbar />
+        <Hero />
+        <About/>
+        <Technologies/>
+        <Experience/>
+        <Projects/>
+      </div>
+    </div>
+  );
+}
+
+export default App
