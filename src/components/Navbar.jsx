82a516615c5ec27b45be8144f3df436249264766
Vidhi Saxena <vidhi094btmae23@igdtuw.ac.in>
2024-12-11 10:58:17 +0530

    added links to socials

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import React from "react";
 import logo from "../assets/VSlogo.png";
-import {FaLinkedin} from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { FaSquareXTwitter } from "react-icons/fa6";
 import { FaInstagram } from "react-icons/fa";
@@ -8,16 +8,40 @@ const Navbar = () => {
   return (
     <nav className="mb-12 flex items-center justify-between py-2">
       <div className="flex flex-shrink-0 items-center">
-        <img src={logo} alt="logo" className="h-20 w-auto"/>
+        <img src={logo} alt="logo" className="h-20 w-auto" />
       </div>
       <div class="m-8 flex items-center justify-center gap-4 text-2xl">
-        <FaLinkedin />
-        <FaGithub />
-        <FaSquareXTwitter />
-        <FaInstagram />
+        <a
+          href={"https://www.linkedin.com/in/vidhi-saxena-86150a243/"}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaLinkedin />
+        </a>
+        <a
+          href={"https://github.com/vidhiisaxena"}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaGithub />
+        </a>
+        <a
+          href={"https://x.com/Vidhisaxena76"}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaSquareXTwitter />
+        </a>
+        <a
+          href={"https://www.instagram.com/vidhiisaxena/"}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaInstagram />
+        </a>
       </div>
     </nav>
   );
-}
+};
 
-export default Navbar
+export default Navbar;
